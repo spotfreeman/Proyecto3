d75295cde41b14974ec73f71493268bf6af59005
R.Oyarzun B <r.oyarzun83@gmail.com>
2023-06-25 02:20:22 -0400

    async

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 // API datos financieros
 const boton = document.getElementById('boton')
 const tarjeta = document.getElementById('tarjeta')
-
 const seleccion = document.getElementById('seleccion')
 
 boton.addEventListener('click', mostrar)
@@ -10,18 +9,43 @@ function mostrar(e) {
     e.preventDefault();
     const opcion = seleccion.value;
     const url = 'https://mindicador.cl/api/' + opcion
-    console.log("Funciona el Boton");
-    console.log(opcion);
 
     tarjeta.innerHTML = `<h3>Origen de datos : ${url}</h3>`
 }
 
 boton.addEventListener('click', async (e) => {
     console.log("segunda funcion")
+    const opcion = seleccion.value;
+    const url = 'https://mindicador.cl/api/' + opcion
+    console.log(opcion)
     e.preventDefault()
 
     const valoresUrl = await fetch(url)
     const valoresSeleccion = await valoresUrl.json()
+    console.log(valoresSeleccion)
+    console.log(valoresSeleccion.serie)
+
+
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 
 
 })
