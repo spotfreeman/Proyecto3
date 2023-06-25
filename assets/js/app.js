c741f9ef755a137d9f423a93531618ae079c35ae
R.Oyarzun B <r.oyarzun83@gmail.com>
2023-06-25 00:53:14 -0400

    Trabajando en Async

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,26 @@
 const boton = document.getElementById('boton')
 const tarjeta = document.getElementById('tarjeta')
 
+const seleccion = document.getElementById('seleccion')
+
 boton.addEventListener('click', mostrar)
 
 function mostrar(e) {
     e.preventDefault();
-    console.log("Funciona el Boton")
+    const opcion = seleccion.value;
+    const url = 'https://mindicador.cl/api/' + opcion
+    console.log("Funciona el Boton");
+    console.log(opcion);
 
-    tarjeta.innerHTML = `<h3>Funciono el Boton</h3>`
-
+    tarjeta.innerHTML = `<h3>Origen de datos : ${url}</h3>`
 }
 
+boton.addEventListener('click', async (e) => {
+    console.log("segunda funcion")
+    e.preventDefault()
+
+    const valoresUrl = await fetch(url)
+    const valoresSeleccion = await valoresUrl.json()
+
+
+})
